c885bf6b4f1ad2a3048b4bf2733678b93f17188b
kaneco <zegomes@gmail.com>
2020-08-04 19:55:43 +0100

    fixed firebase support

--- main.js
+++ main.js
@@ -6,9 +6,13 @@ let readCheckboxes = bookTable.getElementsByTagName("input");
 /* Need a LIVE collection of buttons,  since they are dynamically 
 updated depending on the number of books and because queryselectorall() 
 returns a static collection */
-let firebaseRef = firebase.database().ref("books/");
 
-// Your web app's Firebase configuration
+// Test Books for implementation
+// const book1 = new Book("The Hobbit0", "J.R.R. Tolkien0", 111, true);
+// const book2 = new Book("The Hobbit1", "J.R.R. Tolkien1", 222, true);
+// const book3 = new Book("The Hobbit2", "J.R.R. Tolkien2", 333, false);
+
+// Firebase configuration
 var firebaseConfig = {
 	apiKey: "AIzaSyCJuSaj4JaZOmIyY2RU3sQGDVlWV_Wc_B4",
 	authDomain: "library-1280f.firebaseapp.com",
@@ -21,77 +25,94 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // Get a reference to the database service
-let database = firebase.database();
+let firebaseRef = firebase.database().ref("books");
 
-function saveToDB(book) {
-	firebaseRef.push({
-		title: book.title,
-		author: book.author,
-		pages: book.pages,
-		read: book.read,
-	});
-}
-
-function getFromDB() {
-	myLibrary = []; // clears original stored array to get all books again
-	let booksRef = firebase.database().ref("books/");
-	let i = 0;
-	booksRef.on("value", function (data) {
-		data.val().forEach((book) => {
-			console.log(book.title, book.author, book.pages, book.read);
-			let addedBook = new Book(book.title, book.author, book.pages, book.read); // Creates new book from Form inputs, adds to library and renders to page
-			myLibrary.push(addedBook);
-			i++;
-			//let indexBook = firebase.database().ref("books/" + i);
-			//console.log(firebase.database().ref("books/0").on());
-		});
-	});
-}
-
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, firebaseKey) {
 	//Book object Constructor
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
+	this.firebaseKey = firebaseKey; // stores firebase book ID to be able to remove the correct book from the db
 }
 
+// Toggles the read value of any book on both myLibrary and Firebase
 Book.prototype.toggleReadStatus = function () {
+	readValueRef = firebaseRef.child(this.firebaseKey).child("read");
 	//Toggles read status on a book
 	if (this.read == true || this.read == "true") {
 		this.read = false;
+		readValueRef.set("false"); // update on firebase
 	} else {
 		this.read = true;
+		readValueRef.set("true");
 	}
 };
 
-//function toggleReadSta
-
+//add book to the Library list
 function addBookToLibrary(book) {
-	//add book to the Library list
 	myLibrary.push(book);
 	//add book to Firebase Library
 	saveToDB(book);
 }
 
-function removeBookFromLibrary(index) {
-	myLibrary.splice(index, 1); // Removes book at index and renders the list again with book removed
-	firebase
-		.database()
-		.ref("books/" + index)
-		.remove();
-	render();
+function removeBookFromLibrary(firebaseID) {
+	for (var i = 0; i < myLibrary.length; i++) {
+		if (myLibrary[i].firebaseKey == firebaseID) {
+			myLibrary.splice(i, 1); // Removes book at index and renders the list again with book removed
+			removeFromDB(firebaseID);
+		}
+	}
 }
 
-function deleteRow(index) {
-	//deletes table row
-	document.getElementById("book-list-table").deleteRow(index);
-} // deprecated if favor of removeBookFromLibrary();
+// Firebase Operations / Database integration
 
-function addRow(book, index) {
+// Remove book with a given firebaseID from the database
+function removeFromDB(firebaseID) {
+	firebase
+		.database()
+		.ref("books/" + firebaseID)
+		.remove();
+}
+
+// Save book to firebase with a Unique ID, ordered my time submitted
+function saveToDB(book) {
+	let firebaseID = firebaseRef.push({
+		title: book.title,
+		author: book.author,
+		pages: book.pages,
+		read: book.read,
+	}).key;
+	book.firebaseKey = firebaseID; // Add Firebase Key to object to be able to find and upd items on firebase
+}
+
+// Get all books from the FirebaseDB and synchs with myLibrary Array
+function getFromDB() {
+	myLibrary.length = 0; // clears original stored array to get all books again
+	firebaseRef.on("value", function (data) {
+		data.forEach((book) => {
+			//gets each book from firebase creates new book Object to array in the correct order(chronologically)
+			property = book.val();
+			//console.log(property.title, property.author, property.pages, property.read, book.key);
+			let addedBook = new Book(
+				property.title,
+				property.author,
+				property.pages,
+				property.read,
+				book.key
+			);
+			myLibrary.push(addedBook);
+		});
+	});
+	console.log("getfromDB");
+	console.table(myLibrary);
+}
+
+
+function addRow(book, firebaseID) {
 	// Insert a row at the end of the table
 	let newRow = bookTable.insertRow(-1);
-	newRow.dataset.index = index; // add index number to each row to facilitate searching / deleting books
+	newRow.dataset.key = firebaseID; // add firebaseID number to each row to facilitate searching / deleting books
 	for (const property in book) {
 		if (property == "read") {
 			//Fill Only first 3 Properties (avoid toggleReadStatus()property with this)
@@ -104,44 +125,53 @@ function addRow(book, index) {
 	let readCell = newRow.insertCell(-1);
 	let deleteCell = newRow.insertCell(-1);
 	let readStatus = book.read; //store readStatus property for checkbox
-	let readHTML = `<td><div class="switch"><label><input data-index=${index} type="checkbox" id="book${index}-checkbox" class="filled-in" checked="checked"/><span></span> </label> </div></td>`;
-	let notReadHTML = `<td><div class="switch"><label><input data-index=${index} type="checkbox" id="book${index}-checkbox" class="filled-in"/><span></span> </label> </div></td>`;
-	// readCell.innerHTML = `<td><div class="switch"><label><input type="checkbox" id="book${index}-checkbox" class="filled-in"/><span></span> </label> </div></td>`;
-	deleteCell.innerHTML = `<td><button data-index=${index} class="btn-floating btn-small waves-effect waves-light red" type="button"><i class="material-icons right">delete</i></button></td>`;
+	let readHTML = `<td><div class="switch"><label><input data-key=${firebaseID} type="checkbox" id="book${firebaseID}-checkbox" class="filled-in" checked="checked"/><span></span> </label> </div></td>`;
+	let notReadHTML = `<td><div class="switch"><label><input data-key=${firebaseID} type="checkbox" id="book${firebaseID}-checkbox" class="filled-in"/><span></span> </label> </div></td>`;
+	// readCell.innerHTML = `<td><div class="switch"><label><input type="checkbox" id="book${firebaseID}-checkbox" class="filled-in"/><span></span> </label> </div></td>`;
+	deleteCell.innerHTML = `<td><button data-key=${firebaseID} class="btn-floating btn-small waves-effect waves-light red" type="button"><i class="material-icons right">delete</i></button></td>`;
 	if (readStatus == true || readStatus == "true") {
 		// Change Checkbox depending on book read status
-		//document.getElementById(`book${index}-checkbox`).setAttribute("checked", "checked");
-		//document.getElementById(`book${index}-checkbox`).checked = true;
+		//document.getElementById(`book${firebaseID}-checkbox`).setAttribute("checked", "checked");
+		//document.getElementById(`book${firebaseID}-checkbox`).checked = true;
 		readCell.innerHTML = readHTML;
 	} else {
-		// document.getElementById(`book${index}-checkbox`).removeAttribute("checked");
-		//document.getElementById(`book${index}-checkbox`).checked = false;
+		// document.getElementById(`book${firebaseID}-checkbox`).removeAttribute("checked");
+		//document.getElementById(`book${firebaseID}-checkbox`).checked = false;
 		readCell.innerHTML = notReadHTML;
 	}
 }
 
 function render() {
 	bookTable.innerHTML = ""; // Reset all books already rendered on page
+	console.log(myLibrary);
 	//Render the books currently in myLibrary to the HTML page
-	for (let bookIndex = 0; bookIndex < myLibrary.length; bookIndex++) {
+	for (var bookIndex = 0; bookIndex < myLibrary.length; bookIndex++) {
 		book = myLibrary[bookIndex];
-		addRow(book, bookIndex); // Use bookIndex to have a way to remove / update specific books
+		console.log(book);
+		addRow(book, book.firebaseKey); // Use book.firebaseKey to have a way to remove / update specific books
 	}
-	for (let button of deleteBookButton) {
+	for (var button of deleteBookButton) {
 		// adds a event listener for each delete button of the booklist and get the book index of that book
 		button.addEventListener("click", (e) => {
-			let bookIndex = e.target.parentNode.dataset.index;
-			// gets the data-index attribute from the parent element (2 levels above)
-			removeBookFromLibrary(bookIndex);
+			let bookID = e.target.parentNode.dataset.key;
+			// gets the data-key attribute from the parent element (2 levels above)
+			removeBookFromLibrary(bookID);
+			getFromDB();
+			render();
 		});
 	}
-	for (let checkbox of readCheckboxes) {
+	for (var checkbox of readCheckboxes) {
 		// adds a event listener for each checkbox of the booklist and get the book index of that book
 		checkbox.addEventListener("click", (e) => {
-			let bookIndex = e.target.dataset.index;
+			getFromDB();
+			var bookIndex = myLibrary.findIndex( // find index of the book which matches firebaseKey to toggleReadStatus() that index
+				(book) => book.firebaseKey === e.target.dataset.key
+			);
+			// let bookID = e.target.dataset.key;
 			myLibrary[bookIndex].toggleReadStatus();
 		});
 	}
+	console.log("render ran sucessfully");
 }
 
 addBookForm.addEventListener("submit", function (event) {
@@ -156,6 +186,7 @@ addBookForm.addEventListener("submit", function (event) {
 	modal.style.display = "none";
 	let addedBook = new Book(bookTitle, bookAuthor, bookPages, bookRead); // Creates new book from Form inputs, adds to library and renders to page
 	addBookToLibrary(addedBook);
+	getFromDB();
 	render();
 });
 
@@ -181,5 +212,10 @@ window.onclick = function (event) {
 	}
 };
 
+console.log("before getfromDB");
+console.table(myLibrary);
 getFromDB();
+render();
+console.log("after render");
+console.table(myLibrary);
 render();
